4c9c5a091bcd04e30d95cda7dd07a69813c0ae2f
xravitycapital <xravity@users.noreply.github.com>
2026-06-19 20:57:41 +0800

    feat: prepare analytics and xravity domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://xravity.netlify.app',
+  site: 'https://xravity.xyz',
   integrations: [sitemap()],
   build: {
     assets: 'assets'
